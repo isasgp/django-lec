69e4bedad996b39a706598e610b132f0e6eaa42c
isasgp <isasgp@gmail.com>
2024-07-25 00:36:31 +0900

    week3 complete

--- django_lec/mysite/urls.py
+++ django_lec/mysite/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
     path('<int:content_id>/', views.detail, name='detail'),
 ]
 
